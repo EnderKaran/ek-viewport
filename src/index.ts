4bbaf3c14f7d0284aea4d56dde9bf1ea40252a1d
Ender <ender.karan14@gmail.com>
2026-02-04 23:15:13 +0300

    index güncellendi

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export { useBreakpoint } from './hooks/useBreakpoint';
-export { useMediaQuery } from './hooks/useMediaQuery'; 
+export { useMediaQuery } from './hooks/useMediaQuery';
 export { useOrientation } from './hooks/useOrientation';
 
 export { Show } from './components/Show';
 export { Hide } from './components/Hide';
+export { DebugOverlay } from './components/DebugOverlay';
 
 export type { BreakpointKey } from './utils/constants';
