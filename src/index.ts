d54faa83086f1c879bd495ea9bf7bb31f5392ae9
Ender <ender.karan14@gmail.com>
2026-02-04 23:21:35 +0300

    feat: implement ViewportProvider, useWindowSize, and useScrollDirection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+// Hooks
 export { useBreakpoint } from './hooks/useBreakpoint';
 export { useMediaQuery } from './hooks/useMediaQuery';
 export { useOrientation } from './hooks/useOrientation';
+export { useWindowSize } from './hooks/useWindowSize';
+export { useScrollDirection } from './hooks/useScrollDirection';
 
+// Components & Provider
 export { Show } from './components/Show';
 export { Hide } from './components/Hide';
 export { DebugOverlay } from './components/DebugOverlay';
+export { ViewportProvider, useViewportConfig } from './context/ViewportContext';
 
+// Types
 export type { BreakpointKey } from './utils/constants';
