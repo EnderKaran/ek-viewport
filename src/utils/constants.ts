16e3305f420487ebe9b4305378afccbc75629cad
Ender <ender.karan14@gmail.com>
2026-02-04 23:13:47 +0300

    constant.ts breakpoint fonksiyonu yazildi

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,15 @@ export const defaultBreakpoints = {
     lg: 1024,
     xl: 1280,
     "2xl": 1536,
-} as const ;
+} as const;
+
+export const getCurrentBreakpoint = (width: number): string => {
+    if (width >= defaultBreakpoints['2xl']) return '2xl';
+    if (width >= defaultBreakpoints.xl) return 'xl';
+    if (width >= defaultBreakpoints.lg) return 'lg';
+    if (width >= defaultBreakpoints.md) return 'md';
+    if (width >= defaultBreakpoints.sm) return 'sm';
+    return 'xs'; // 640px altı için
+  };
 
 export type BreakpointKey = keyof typeof defaultBreakpoints;
